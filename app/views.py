7c4297e5a68e7d0f1ec9b0269dab96e84cc03ab4
Micah <micahkimathi@gmail.com>
2019-05-30 15:29:24 +0300

    Created the config.py file inside the instance folder

--- app/views.py
+++ app/views.py
@@ -2,10 +2,18 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/')
-def index():
+@app.route('/blog/<int:blog_id>')
+def blog(blog_id):
     
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    return render_template('blog.html',id = blog_id)
+
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    
+    title = 'Home - Welcome to Blogs'
+    return render_template('index.html', title = title)
