003e3cc46236c4806d97f2aee0b7118134313e46
Micah <micahkimathi@gmail.com>
2019-06-02 22:13:44 +0300

    Initialized the bootstrap class in the __init__.py file

--- app/views.py
+++ app/views.py
@@ -2,6 +2,13 @@ from flask import render_template
 from app import app
 
 # Views
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    message = 'Blogs'
+    return render_template('index.html',message = message)
 @app.route('/blog/<int:blog_id>')
 def blog(blog_id):
     
