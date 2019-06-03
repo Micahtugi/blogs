c0a1b1f156179304f5fd22d9e937038562550fbe
Micah <micahkimathi@gmail.com>
2019-06-03 11:11:38 +0300

    Created the auth folder inside the app directory

--- app/main/views.py
+++ app/main/views.py
@@ -2,16 +2,16 @@ from flask import render_template
 from . import main
 from .forms import ReviewForm
 from ..models import Review
-Review = review.Review
+# Review = review.Review
 # Views
-@app.route('/')
+@main.route('/')
 def index():
     '''
     View root page function that returns the index page and its data
     '''
     message = 'Blogs'
     return render_template('index.html',message = message)
-@app.route('/blog/<int:blog_id>')
+@main.route('/blog/<int:blog_id>')
 def blog(blog_id):
     
     '''
--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 class Config:
     BLOG_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('Tugi')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://micah:tugi@localhost/blogs'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 class ProdConfig(Config):
     pass
