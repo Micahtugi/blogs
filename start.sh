6550c3b6839e2c68c07f910c8b46ca7d48ebf1a9
Micah <micahkimathi@gmail.com>
2019-06-04 14:08:23 +0300

    Exported the database uri in the start.sh file

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 export SECRET_KEY='tugi'
+export DATABASE_URI='postgresql+psycopg2://micah:tugi@localhost/blogs'
 # export BLOG_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
 python3.6 manage.py server
