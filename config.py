1b5b9720673e99e9f683aaa458b0ae4197a81967
Micah <micahkimathi@gmail.com>
2019-06-04 14:11:39 +0300

    Final changes

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 class Config:
-    # BLOG_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
+    BLOG_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://micah:tugi@localhost/blogs'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
