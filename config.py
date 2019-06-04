4c3f32d32d9dc0964e0b2cf517327a8c579265dd
Micah <micahkimathi@gmail.com>
2019-06-04 16:07:39 +0300

    Fix Migrations

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://micah:tugi@localhost/blogs2'
+    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URI")
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://micah:tugi@localhost/blogs2'
