6b8a465fe33764964fba937c376403be70d75866
Micah <micahkimathi@gmail.com>
2019-06-04 16:13:01 +0300

    Fix Migrations

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 export SECRET_KEY='tugi'
-export DATABASE_URI='postgresql+psycopg2://micah:tugi@localhost/blogs2'
+# export DATABASE_URI='postgresql+psycopg2://micah:tugi@localhost/blogs2'
 # export BLOG_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
 python3.6 manage.py server
