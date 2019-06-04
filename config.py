ea65b62d274d70cabed9369736cef6c8744ecb00
Micah <micahkimathi@gmail.com>
2019-06-04 16:57:53 +0300

    Created new addons

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI=os.environ.get("HEROKU_POSTGRESQL_WHITE_URL")
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://micah:tugi@localhost/blogs2'
