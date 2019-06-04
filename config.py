d88a8283ce75ae3b66c4dc42333e717d910e8553
Micah <micahkimathi@gmail.com>
2019-06-04 09:20:31 +0300

    Deployment to heroku

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
 
 class DevConfig(Config):
     DEBUG = True
